f27836ec63442b64a1abe4848a5036edaf1f86b5
sharibarboza <barbozashari@gmail.com>
2017-03-14 22:45:32 -0600

    Add comments

--- parse.py
+++ parse.py
@@ -6,6 +6,11 @@ root = tree.getroot()
 
 
 def strip_text(string, tag):
+    """Removes tags from string
+
+    :param string: tag + text string
+    :param tag: tag label
+    """
     string = string.decode('ascii')
     opening = "<%s>" % (tag)
     closing = "</%s>" % (tag)
@@ -17,15 +22,31 @@ def strip_text(string, tag):
     return string
 
 def convert_quote(string):
+    """Escape html for double quotation marks
+
+    :param string: text inside tag
+    """
     return string.replace('"', " &quot ")	
 
 def filter_len(term):
+    """Ignore terms of length 2 or less
+
+    :param term: string token
+    """
     return len(term) > 2
 
 def filter_html(term):
+    """Ignore all special characters coded as &#number
+
+    :param term: string token
+    """
     return False if '&#' in term else True
 
 def filter_string(string):
+    """Remove unwanted characters from string text
+
+    :param string: tag + text
+    """
     string = convert_quote(string)
     tokens = ' '.join(re.split(';', string))
     string = tokens.replace(',', '')
@@ -34,11 +55,20 @@ def filter_string(string):
     return ' '.join(tokens)
 
 def filter_tokens(string):
+    """Remove unwanted terms from tokens and convert to lowercase
+
+    :param string: filtered string
+    """
     tokens = re.split('[\W]', string)
     tokens = list(filter(filter_len, tokens))
     return [token.lower() for token in tokens]
 
 def get_terms(string, tag):
+    """Tokenize the string and return the tokens in a list
+
+    :param string: tag + text
+    :param tag: tag label
+    """
     tokens = []
     raw_txt = strip_text(ET.tostring(string), tag)
     filtered_txt = filter_string(raw_txt)
@@ -51,16 +81,18 @@ def get_terms(string, tag):
 def main():
     # Phase 1: Preparing data files
     outfile1 = 'terms.txt'
-    outfile2 = 'dates.txt'
-    outfile3 = 'tweets.txt'
-
     f1 = open(outfile1, 'w')
-    f2 = open(outfile2, 'w')
-    f3 = open(outfile3, 'w')
-
     t_str = "t-%s:%s\n"
     n_str = "n-%s:%s\n"
     l_str = "l-%s:%s\n"
+
+    outfile2 = 'dates.txt'
+    f2 = open(outfile2, 'w')
+    d_str = "%s:%s\n"
+
+    outfile3 = 'tweets.txt'
+    f3 = open(outfile3, 'w')
+    s_str = "%s:%s\n"
 
     # Scan through each tweet record stored in 'status' tags in xml format
     
@@ -88,15 +120,13 @@ def main():
             f1.write(l_str % (term, id_num))
 	    	
         # -------------------------TASK-2-------------------------------
-        date_str = "%s:%s\n"
         date = status.findtext('created_at')
-        f2.write(date_str % (date, id_num))
+        f2.write(d_str % (date, id_num))
 
         # -------------------------TASK-3-------------------------------
-        stat_str = "%s:%s\n"
         record = ET.tostring(status)
         record = record.decode('ascii').rstrip('\n')
-        f3.write(stat_str % (id_num, record))
+        f3.write(s_str % (id_num, record))
 
     f1.close()
     f2.close()
