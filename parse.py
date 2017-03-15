3278bbf91e692b48ab1906c07c5337dea739cb65
sharibarboza <barbozashari@gmail.com>
2017-03-14 20:45:45 -0600

    Get tweet records

--- parse.py
+++ parse.py
@@ -40,6 +40,7 @@ def get_terms(string, tag):
     filtered_txt = filter_string(raw_txt)
     return filter_tokens(filtered_txt)
 
+
 #------------------------------MAIN---------------------------------
 
 def main():
@@ -49,34 +50,32 @@ def main():
 
     f1 = open(outfile1, 'w')
     f2 = open(outfile2, 'w')
+    f3 = open(outfile3, 'w')
+
+    t_str = "t-%s:%s\n"
+    n_str = "n-%s:%s\n"
+    l_str = "l-%s:%s\n"
 
     for status in root.iter('status'):
-        id_num = status.findtext('id')
+        id_num = status.findtext('id')       
+        user = status.find('user')
+        text = status.find('text')
 
     # -------------------------TASK 1-------------------------------
         # Get text terms
-        t_str = "t-%s:%s\n"
-        text = status.find('text')
         t_terms = get_terms(text, 'text')
-
         for term in t_terms:
             f1.write(t_str % (term, id_num))
 
-        user = status.find('user')
-
         # Get name terms
-        n_str = "n-%s:%s\n"
         name = user[0]
-        n_terms = get_terms(name, 'name')
-        
+        n_terms = get_terms(name, 'name')      
         for term in n_terms:
             f1.write(n_str % (term, id_num))
 
         # Get location terms
-        l_str = "l-%s:%s\n"
         loc = user[1]
         l_terms = get_terms(loc, 'location')
-
         for term in l_terms:
             f1.write(l_str % (term, id_num))
 	    	
@@ -85,20 +84,16 @@ def main():
         date = status.findtext('created_at')
         f2.write(date_str % (date, id_num))
 
-
-
-
-
-
-
-
-
-
-
     # -------------------------TASK 3-------------------------------
+        stat_str = "%s:%s\n"
+        record = ET.tostring(status)
+        record = record.decode('ascii').rstrip('\n')
+        f3.write(stat_str % (id_num, record))
 
     f1.close()
     f2.close()
+    f3.close()
+
 
 if __name__ == "__main__":
 	main()
