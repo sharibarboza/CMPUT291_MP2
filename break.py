ea7045fbf5c7f6e5d4bc31ab8dd0cc558601e802
sharibarboza <barbozashari@gmail.com>
2017-03-18 23:36:30 -0600

    Add break.py

---
+++ break.py
@@ -0,0 +1,38 @@
+"""
+Part of Phase 2
+Prepare data files for loading
+Place keys on first line and values on second line
+Remove any backslashes
+Write changed data back to file
+"""
+
+infiles = ['tweets.txt', 'dates.txt', 'terms.txt']
+
+for file in infiles:
+    # Open .txt data file
+    f1 = open(file, 'r')
+    records = f1.read().split('\n')
+    f1.close()
+    new_records = [] 
+
+    # Change output of record
+    for record in records:
+        if len(record) > 0:
+            # Get key and value
+            data = record.split(':', 1)
+            key, value = data[0], data[1]
+
+            # Remove backslashes
+            if '\\' in value:
+            	value = value.replace('\\', '\\\\')
+            lines = "%s\n%s\n" % (key, value)
+            new_records.append(lines)
+
+    # Remove newline from end
+    new_records[-1] = new_records[-1].rstrip('\n')
+
+    # Write back to .txt file
+    f2 = open(file, 'w')
+    for record in new_records:
+        f2.write(record)
+    f2.close()
