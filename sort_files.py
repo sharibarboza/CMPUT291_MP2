c02b454127990ddb4c5c54ead1a417ed038d9b76
sharibarboza <barbozashari@gmail.com>
2017-03-29 12:44:44 -0600

    Remove separate phase 2 files

--- sort_files.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Sort files and get unique rows from command line
-os.system("sort terms.txt -u -o terms.txt")
-os.system("sort dates.txt -u -o dates.txt")
-os.system("sort tweets.txt -u -o tweets.txt")
